810322a0b1e79a9ce7531d29b7b1e0bdf5ff630d
Zenith Zack <faldund@outlook.com>
2021-11-08 17:52:46 -0500

    structure of card with icon

--- src/components/IconFigureCard.js
+++ src/components/IconFigureCard.js
@@ -1,6 +1,42 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from "styled-components";
+import theme from "../styling-properties/propSchoolsCard";
 
-const StyledCard = styled.div`
-    display:
+const Card = styled.div`
+    height: ${(p) => p.theme.cardHeight}
+    width: ${(p) => p.theme.cardWidth}
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: ${(p) => p.theme.cardBorderRadius};
+    background: ${(p) => p.theme.cardColorBackground};
 `
+
+const Label = styled.div`
+    color: ${(p) => p.theme.labelColor};
+    font-size: ${(p) => p.theme.labelFontSize};
+`
+
+const Value = styled.div`
+    color: ${(p) => p.theme.valueColor};
+    font-size: ${(p) => p.theme.valueFontSize};
+`
+
+const Icon = styled.span`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-raidus: ${(p) => p.theme.borderRadiusIcon};
+`
+
+const IconFigureCard = function(){
+    return(
+        <Card>
+            <Label></Label>
+            <Label></Label>
+            <Icon></Icon>
+        </Card>
+    );
+}
+
+export default IconFigureCard;
---
+++ src/styling-properties/propSchoolsCard.js
@@ -0,0 +1,37 @@
+import { rgba } from "polished";
+
+const colorTitle = rgba("white", 0.57);
+const colorContent = rgba("white", 0.96);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    // Setting an id to this theme
+    id: "dark",
+
+    // Font Family
+    fontFamily: "Poppins",
+
+    // Properties of a Card
+    cardHeight: "35rem",
+    cardWidth: "35rem",
+    borderRadius: "1rem",
+    colorCardBackground: "#1B1B1B",
+
+    // Properties of Card Title
+    cardTitleFontSize: "2rem",
+    cardTitlePaddingTop: "2rem",
+    cardTitlePaddingRight: "2rem",
+    cardTitlePaddingBottom: "2rem",
+    cardTitlePaddingLeft: "2rem",
+    colorTitle: colorTitle,
+
+    // Properties of Card Content
+    cardContentPaddingTop: "None",
+    cardContentPaddingRight: "None",
+    cardContentPaddingBottom: "None",
+    cardContentPaddingLeft: "None",
+
+    // Properties of Card Content (Text other than title)
+    cardContentFontSize: "2em",
+    colorContent: colorContent,
+};
