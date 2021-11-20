7d3d297638e9537b0d99c87b7f49b84d30a97526
Zenith Zack <faldund@outlook.com>
2021-11-20 13:28:21 -0500

    added routers and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
 import StatsDashboard from './components/StatsDashboard';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <StatsDashboard></StatsDashboard>
-    </div>
+    <Router>
+      <div className="App">
+      {/* <StatsDashboard></StatsDashboard> */}
+        <ul className="App-header">
+          <li>
+            <Link to="/live">Live</Link>
+          </li>
+          <li>
+            <Link to="/yearly">Yearly</Link>
+          </li>
+          <li>
+            <Link to="/trends">Trends</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path='/live' component={StatsDashboard}></Route>
+          <Route exact path='/yearly' component={}></Route>
+          <Route exact path='/trends' component={}></Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
