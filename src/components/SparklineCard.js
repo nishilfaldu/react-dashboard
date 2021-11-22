d76ad1745fb0b2338f20c4ec2bd2c24ab01780c7
Zenith Zack <faldund@outlook.com>
2021-11-22 16:12:22 -0500

    new sparkline component for trial

---
+++ src/components/SparklineCard.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled, { ThemeProvider, css} from "styled-components";
+import Chart from 'react-apexcharts';
+import { useState } from 'react';
+
+const SparklineCard = function(props){
+
+    const [options, setOptions] = useState({
+        chart: {
+            id: 'spark1',
+            group: 'sparks',
+            type: 'line',
+            height: 80,
+            sparkline: {
+              enabled: true
+            },
+            dropShadow: {
+              enabled: true,
+              top: 1,
+              left: 1,
+              blur: 2,
+              opacity: 0.2,
+            }
+          },
+          series: [{
+            data: [25, 66, 41, 59, 25, 44, 12, 36, 9, 21]
+          }],
+          stroke: {
+            curve: 'smooth'
+          },
+          markers: {
+            size: 0
+          },
+          grid: {
+            padding: {
+              top: 20,
+              bottom: 10,
+              left: 110
+            }
+          },
+          colors: ['#fff'],
+          tooltip: {
+            x: {
+              show: false
+            },
+            y: {
+              title: {
+                formatter: function formatter(val) {
+                  return '';
+                }
+              }
+            }
+          }
+    });
+
+    return(
+        <div>
+            
+        </div>
+    );
+}
