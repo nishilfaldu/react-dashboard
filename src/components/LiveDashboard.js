be1957379286e416d2ce005067de15f0bc9050ac
Zenith Zack <faldund@outlook.com>
2021-11-22 15:33:36 -0500

    styled row flex-display for live

--- src/components/LiveDashboard.js
+++ src/components/LiveDashboard.js
@@ -2,12 +2,20 @@ import styled from 'styled-components';
 import PieChartCard from './PieChartCard';
 import BarChartCard from './BarChartCard';
 
+const StyledRow = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    padding-bottom: 1rem;
+`
+
 const LiveDashboard = function(){
     return(
         <div>
+        <StyledRow>
             <PieChartCard cardTitle="Ethnicities" cardLabel="Count" cardValue="5" chartType={'pie'}></PieChartCard>
             <BarChartCard cardTitle="Countries" cardLabel="Count" cardValue="250"></BarChartCard>
             <PieChartCard cardTitle="Gender" cardLabel="Female" cardValue="25%" chartType={'donut'}></PieChartCard>
+        </StyledRow>
         </div>
     );
 }
