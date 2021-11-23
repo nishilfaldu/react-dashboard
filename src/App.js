21a456fb00ea754e82ab93f5bf26b5054721dbb6
Zenith Zack <faldund@outlook.com>
2021-11-22 20:09:28 -0500

    something in app.js

--- src/App.js
+++ src/App.js
@@ -12,21 +12,6 @@ import NavRoutes from './components/router/NavRoutes';
 function App() {
   return (
     <div className="App">
-      {/* <Router>
-        <div>
-          <div className="Navbar">
-              <Link to="/live" style={{ textDecoration: 'none' }}><NavButton label="Live"></NavButton></Link>
-              <Link to="/yearly" style={{ textDecoration: 'none' }}><NavButton label="Yearly"></NavButton></Link>
-              <Link to="/trends" style={{ textDecoration: 'none' }}><NavButton label="Trends"></NavButton></Link>
-          </div>
-
-          <Switch>
-            <Route exact path='/live' component={YearlyDashboard}></Route>
-            <Route exact path='/yearly' component={YearlyDashboard}></Route>
-            <Route exact path='/trends' component={YearlyDashboard}></Route>
-          </Switch>
-        </div>
-      </Router> */}
       <NavRoutes></NavRoutes>
     </div>
     
