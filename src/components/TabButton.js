94d4b249a6da608e2da49a38df6df6c459242d44
Zenith Zack <faldund@outlook.com>
2021-11-23 14:55:29 -0500

    css file for tab buttons

--- src/components/TabButton.js
+++ src/components/TabButton.js
@@ -4,50 +4,42 @@ import styled from "styled-components";
 import CheckIcon from '@mui/icons-material/Check';
 import HowToRegIcon from '@mui/icons-material/HowToReg';
 
-
-const SpanButton = styled.span`
-    positive: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 2rem;
-    color: #ffffff;
-    height: 3rem;
-    padding: 0;
-    border-radius: 2rem;
-`
-
-const SpanButtonIcon = styled.span`
-    font-size: 1.5rem;
-    transition: all 0.35s;
-`
-
-const SpanButtonText = styled.span`
-    position: absolute;
-    left: 0.5rem;
-    top: 50%;
-    transform: translateY(-50%) translateX(1.25rem);
-    // both props below hides an elements
-    visibility: hidden;
-    opacity: 0;
-    font-size: 0.875rem;
-    color: #fff;
-    transition: all 0.35s;
-`
-
 const TabButton = function(props){
     return(
-        <div>
+        <div styled={{backgroundColor: '#FF0000'}}>
             <label for='input-1'>
                 <input
                 type='radio'
                 id='input-1'
                 name='group'
-                // disappear element by scale(0)
-                style={{position: 'absolute',transform: 'scale(0)'}}/>
+                />
                 <span className='btn'>
-                    <span className='btn-icon uil uil-chart'/>
+                    <span className='btn-icon'/>
                     <span className='btn-text'>Registered</span>
+                </span>
+            </label>
+
+            <label for='input-2'>
+                <input
+                type='radio'
+                id='input-2'
+                name='group'
+                />
+                <span className='btn'>
+                    <span className='btn-icon'/>
+                    <span className='btn-text'>Confirmed</span>
+                </span>
+            </label>
+
+            <label for='input-3'>
+                <input
+                type='radio'
+                id='input-3'
+                name='group'
+                />
+                <span className='btn'>
+                    <span className='btn-icon'/>
+                    <span className='btn-text'>Checked In</span>
                 </span>
             </label>
         </div>
