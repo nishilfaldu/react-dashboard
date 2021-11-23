1043a1679b7e711abdfc2cf4bdbc3b3aa2b32905
Zenith Zack <faldund@outlook.com>
2021-11-23 14:01:23 -0500

    styled span button aded

--- src/components/TabButton.js
+++ src/components/TabButton.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import styled from "styled-components";
 
+const SpanButton = styled.span`
+    positive: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    color: #ffffff;
+    height: 3rem;
+    padding: 0;
+    border-radius: 2rem;
+`
+
 const TabButton = function(props){
     return(
         <div>
@@ -10,10 +22,12 @@ const TabButton = function(props){
                 id='input-1'
                 name='group'
                 />
-                <span className='btn'>
+                <SpanButton>
+                {/* <span className='btn'> */}
                     <span className='btn-icon uil uil-chart'/>
                     <span className='btn-text'>Registered</span>
-                </span>
+                {/* </span> */}
+                </SpanButton>
             </label>
         </div>
     );
