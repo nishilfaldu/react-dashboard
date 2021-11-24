92de68cd561e0087ccaebaca5106e77d975cb018
Zenith Zack <faldund@outlook.com>
2021-11-24 01:31:56 -0500

    fixed css for tab buttons

--- src/components/LivePage.js
+++ src/components/LivePage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import LiveDashboard from './LiveDashboard';
 import Tab from './Tab'
+import styled from 'styled-components';
+
+const StyledRow = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    padding-bottom: 1rem;
+`
+
 const LivePage = function(props){
     return(
         <div>
-            <Tab></Tab>
+            <StyledRow>
+                <Tab></Tab>
+            </StyledRow>
             <LiveDashboard></LiveDashboard>
         </div>
     );
