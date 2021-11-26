d3fbd0f795d8a4104dca116b0ea3aae6907d4575
Zenith Zack <faldund@outlook.com>
2021-11-25 23:50:22 -0500

    added buttons to trends page

--- src/components/pages/TrendsPage.js
+++ src/components/pages/TrendsPage.js
@@ -1,9 +1,21 @@
 import React from 'react';
 // import TrendsDashboard from './TrendsDashboard';
+import styled from 'styled-components';
+import { useState } from 'react';
+import Tab from '../menus/Tab';
+
+const StyledRow = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    padding-bottom: 1rem;
+`
 
 const TrendsPage = function(props){
     return(
         <div>
+            <StyledRow>
+                <Tab></Tab>
+            </StyledRow>
             {/* <TrendsDashboard></TrendsDashboard> */}
         </div>
     );
