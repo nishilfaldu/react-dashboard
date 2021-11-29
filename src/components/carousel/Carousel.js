666887b494c3c004d6871e13118603bb579d9b51
Zenith Zack <faldund@outlook.com>
2021-11-28 22:02:36 -0500

    basic structure for carousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const Carousel = function(){
+const Carousel = function({ children }){
     return(
-        <div></div>
+        <div>
+            <div style={{ transform: 'translateX(0%)'}}>
+                {React.Children.map(children, (child, index) => {
+                    return React.cloneElement(child, { width: "100%" });
+                })}
+            </div>
+        </div>
     );
 }
 
