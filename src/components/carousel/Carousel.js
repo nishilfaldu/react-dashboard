29863fe0e34c187cfb2636889a5885ce6c0f4515
Zenith Zack <faldund@outlook.com>
2021-11-28 21:54:24 -0500

    file structure for carousel

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Carousel = function(){
+    return(
+        <div></div>
+    );
+}
+
+export default Carousel;
---
+++ src/components/carousel/CarouselItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CarouselItem = function(){
+    return(
+        <div></div>
+    );
+}
+
+export default CarouselItem;
