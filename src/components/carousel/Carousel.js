ea8586ed8a38ef118e728047a9ec7bc59a8cbd8a
Zenith Zack <faldund@outlook.com>
2021-11-28 22:33:29 -0500

    configured active state of carousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../../assets/css/Carousel.css';
+import { useState } from 'react';
 
 const Carousel = function({ children }){
+
+    const [activeIndex, setActiveIndex] = useState(0);
+
     return(
         <div className="carousel">
-            <div className="inner" style={{ transform: 'translateX(0%)'}}>
+            <div className="inner" style={{ transform: `translateX(-${activeIndex*100})` }}>
                 {React.Children.map(children, (child, index) => {
                     return React.cloneElement(child, { width: "100%" });
                 })}
