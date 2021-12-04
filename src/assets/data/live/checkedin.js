3e1d5a47e71ebda7534b71a083a9d293e1ef4151
Zenith Zack <faldund@outlook.com>
2021-12-03 23:47:47 -0500

    added checkedin data

---
+++ src/assets/data/live/checkedin.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    numParticipants: 500,
+    numRegistrants: 500,
+    numSchools: 50,
+    numStates: 50,
+    numCountries: 55,
+    numUsersLattice: 555,
+    numFemales: 400,
+    numUsersDiscord: 800,
+
+    numMajors: 5,
+    labelMajors: ['ME', 'EE', 'CS', 'CE', 'BME'],
+    seriesMajors: ['20','20','20','20','20'],
+
+    labelForCard: 'TBD',
+    labelAges: ['<18','18','19','20','21','22','23','23+'],
+    seriesAge: [30, 100, 35, 110, 40, 56, 100, 65],
+
+    totalHackathons: 250,
+    labelHackExperience: ['0','1','2','3','4','5','5+'],
+    seriesHackExperience: [30, 100, 35, 110, 40, 56, 100, 65],
+};
