6c0bff7601c5fb6103cc6ad18171c55e224053a1
Zenith Zack <faldund@outlook.com>
2021-12-07 23:09:34 -0500

    demographics file

--- src/assets/data/trends/demographics.js
+++ src/assets/data/trends/demographics.js
@@ -1,63 +1,103 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     hackSpring2014 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Asian', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
     hackFall2014 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
     hackSpring2015 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
     hackSpring2016 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
     hackFall2016 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
     hackSpring2017 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
     hackSpring2018 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
     hackSpring2019 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
     hackSpring2020 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
     hackSpring2021 : {
-        seriesRegistrants : [],
-        seriesParticipants: [],
-        seriesSubmissions: [],
-        seriesSponsors: []
+        labelAges: ['<18','18','19','20','21','22','23','23+'],
+        seriesAge: [45, 23, 43, 54, 73, 27, 52, 44],
+        labelEthnicities: ['Hispanic or Latino', 'American Indian or Alaska Native', 'Black or African American', 'Native Hawaiian or Other Pacific Islander', 'White', 'Not Specified'],
+        seriesEthnicities: [67, 97, 107, 27, 57, 65, 54],
+        labelGenders: ['Male', 'Female', 'Non-binary or Third Gender', 'Other', 'Not Specified'],
+        seriesGender: [123, 47, 75, 46, 73],
+        labelStates: ['OH', 'CA', 'MI', 'TX', 'NV'],
+        seriesStates: [23, 76, 89, 45, 63]
     },
 };
--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -25,7 +25,7 @@ const Carousel = function({ children }){
                 })}
             </div>
 
-            <div className='indicators'>
+            <div className='indicators' style={{paddingLeft: '10rem'}}>
                 {React.Children.map(children, (child, index) => {
                     return(
                         // <button 
