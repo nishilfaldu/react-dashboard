4a16b1f1d2a1e8f2f07b0c0b537ef886c7f60398
Zenith Zack <faldund@outlook.com>
2021-12-08 18:36:40 -0500

    removed states chart and aligned three toher

--- src/components/dashboards/TrendsDashboard.js
+++ src/components/dashboards/TrendsDashboard.js
@@ -4,32 +4,37 @@ import styled from 'styled-components';
 import BarChartCard from '../cards/charts/BarChartCard';
 import PieChartCard from '../cards/charts/PieChartCard';
 
-const StyledRow = styled.div`
-    display: flex;
-    padding-top: 5rem;
-    justify-content: center;
-    padding-left: 15rem;
-    flex-direction: row;
-`
-
 // const StyledRow = styled.div`
 //     display: flex;
+//     flex-direction: row;
+//     padding-top: 1.5rem;
 //     justify-content: space-evenly;
-//     padding-bottom: 1rem;
+//     align-items: center;
+//     padding-left: 14rem;
 // `
+
+const StyledRow = styled.div`
+    display: flex;
+    justify-content: space-between;
+    // padding-bottom: 1rem;
+    padding-left: 15rem;
+    padding-top: 1.5rem;
+    // height: 15rem;
+`
 
 const TrendsDashboard = function(props){
     return(
         <div>
             <StyledRow>
+                <PieChartCard cardTitle="Gender" cardLabel="Female" cardValue='5' chartType={'pie'} labelData={props.data.labelGenders} seriesData={props.data.seriesGender}></PieChartCard>
                 <BarChartCard cardTitle="Age" cardLabel="Current" cardValue='18+' labelData={props.data.labelAges} seriesData={props.data.seriesAge}></BarChartCard>
                 <PieChartCard cardTitle="Ethnicities" cardLabel="Count" cardValue='7' chartType={'pie'} labelData={props.data.labelEthnicities} seriesData={props.data.seriesEthnicities}></PieChartCard>
             </StyledRow>
 
-            <StyledRow>
-                <PieChartCard cardTitle="Gender" cardLabel="Female" cardValue={props.genderVal} chartType={'donut'} labelData={props.data.labelGenders} seriesData={props.data.seriesGender}></PieChartCard>
-                <BarChartCard cardTitle="States" cardLabel="Count" cardValue='5' labelData={props.data.labelStates} seriesData={props.data.seriesStates}></BarChartCard>
-            </StyledRow>
+            {/* <StyledRow>
+                
+                {/* <BarChartCard cardTitle="States" cardLabel="Count" cardValue='5' labelData={props.data.labelStates} seriesData={props.data.seriesStates}></BarChartCard> */}
+            {/* </StyledRow> */} 
             {/* <SparklineCard></SparklineCard> */}
         </div>
     );
