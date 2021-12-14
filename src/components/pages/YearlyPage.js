b4ecc4d3796c74416326562fc139a6abb9908184
Zenith Zack <faldund@outlook.com>
2021-12-13 22:40:15 -0500

    added all yearly dashboards unstyled

--- src/components/pages/YearlyPage.js
+++ src/components/pages/YearlyPage.js
@@ -3,6 +3,9 @@ import YearlyDashboard from '../dashboards/YearlyDashboard';
 import Navbar from '../navbar/Navbar';
 import annualData from '../../assets/data/annual/annual';
 import styled from 'styled-components';
+import Carousel from '../carousel/Carousel';
+import logosData from '../../assets/data/images/logos';
+import CarouselItem from '../carousel/CarouselItem';
 
 const StyledRow = styled.div`
     display: flex;
@@ -16,9 +19,15 @@ const YearlyPage = function(props){
     return(
         <div>
             <Navbar/>
-            {/* <StyledRow> */}
-            <YearlyDashboard data={annualData['hackSpring2014']}></YearlyDashboard>
-            {/* </StyledRow> */}
+            <Carousel>
+            {
+                Object.keys(logosData).map(
+                    (key, index) => {
+                        return (<CarouselItem key={index} src={logosData[key]['imgUrl']} alt={logosData[key]['imgAlt']}><YearlyDashboard data={annualData[key]}></YearlyDashboard></CarouselItem>);
+                    }
+                )
+            }
+        </Carousel>
         </div>
     );
 }
